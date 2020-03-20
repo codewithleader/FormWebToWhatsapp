4049464478502369e0ab044cfaa61dc13f63830e
ElisPerez <62060473+ElisPerez@users.noreply.github.com>
2020-03-19 19:47:36 -0500

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,12 +1,12 @@
 document.querySelector('#submit').addEventListener('click',function(){
-    var cliente = document.querySelector('#cliente').Value;
-    var fecha = document.querySelector('#fecha').Value;
-    var hora = document.querySelector('#hora').Value;
-    var barbero = document.querySelector('#barbero').Value;
-    var servicio = document.querySelector('#servicio').Value;
-
-    var url = "https://api.whatsapp.com/send?phone=573105010573&text=*_Barberia Lider_*%0A*Reservas*%0A%0A*¿Cual es tu nombre?*%0A"+encodeURIComponent(cliente)+"%0A*Indica la fecha de tu reserva*%0A"+encodeURIComponent(fecha)+"%0A*Indica la hora de tu reserva*%0A"+encodeURIComponent(hora)+"%0A*Barbero de preferencia*%0A"+encodeURIComponent(barbero)+"%0A*¿Cual es el servicio que se desea realizar?*%0A"+encodeURIComponent(servicio)
     
+    let cliente = document.querySelector('#cliente').value;
+    let fecha = document.querySelector('#fecha').value;
+    let hora = document.querySelector('#hora').value;
+    let barbero = document.querySelector('#barbero').value;
+    let servicio = document.querySelector('#servicio').value;
+
+    let url = "https://api.whatsapp.com/send?phone=573105010573&text=*_Barberia Lider_*%0A*Reservas*%0A%0A*¿Cual es tu nombre?*%0A" + cliente + "%0A*Indica la fecha de tu reserva*%0A" + fecha + "%0A*Indica la hora de tu reserva*%0A" + hora + "%0A*Barbero de preferencia*%0A" + barbero + "%0A*¿Cual es el servicio que se desea realizar?*%0A" + servicio;
     window.open(url);
 
 });

1748d603af1e09242c8118a3ab47ebbbdd65b4d9
Gerson Umanzor Alberto <45671861+gbumanzor@users.noreply.github.com>
2020-03-22 09:55:00 -0600

    Update form.js
    Improve readability

--- js/form.js
+++ js/form.js
@@ -1,12 +1,20 @@
-document.querySelector('#submit').addEventListener('click',function(){
-    
-    let cliente = document.querySelector('#cliente').value;
-    let fecha = document.querySelector('#fecha').value;
-    let hora = document.querySelector('#hora').value;
-    let barbero = document.querySelector('#barbero').value;
-    let servicio = document.querySelector('#servicio').value;
+document.querySelector("#submit").addEventListener("click", function() {
+  const cliente = document.querySelector("#cliente").value;
+  const fecha = document.querySelector("#fecha").value;
+  const hora = document.querySelector("#hora").value;
+  const barbero = document.querySelector("#barbero").value;
+  const servicio = document.querySelector("#servicio").value;
 
-    let url = "https://api.whatsapp.com/send?phone=573105010573&text=*_Barberia Lider_*%0A*Reservas*%0A%0A*¿Cual es tu nombre?*%0A" + cliente + "%0A*Indica la fecha de tu reserva*%0A" + fecha + "%0A*Indica la hora de tu reserva*%0A" + hora + "%0A*Barbero de preferencia*%0A" + barbero + "%0A*¿Cual es el servicio que se desea realizar?*%0A" + servicio;
-    window.open(url);
-
+  const url = `https://api.whatsapp.com/send?phone=573105010573&text=*_Barberia Lider_*%0A*Reservas*%0A%0A*¿Cual es tu nombre?*%0A
+    ${cliente}
+    %0A*Indica la fecha de tu reserva*%0A
+    ${fecha}
+    %0A*Indica la hora de tu reserva*%0A
+    ${hora}
+    %0A*Barbero de preferencia*%0A
+    ${barbero}
+    %0A*¿Cual es el servicio que se desea realizar?*%0A
+    ${servicio}`;
+  // window.open(url);
+  fetch(url).then(() => console.log("Message was sent to WhatsApp"));
 });

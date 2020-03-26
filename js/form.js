bdf64655f8dda82885c68e85c86e71be4281ccc0
ElisPerez <62060473+ElisPerez@users.noreply.github.com>
2020-03-26 16:11:01 -0500

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,6 +1,9 @@
 document.querySelector('#submit').addEventListener('click', (e)=>{
     e.preventDefault();
 
+    //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+	let telefono = "573105010573";
+    
     let cliente = document.querySelector('#cliente').value;
     let fecha = document.querySelector('#fecha').value;
     let hora = document.querySelector('#hora').value;
@@ -11,7 +14,7 @@ document.querySelector('#submit').addEventListener('click', (e)=>{
     resp.classList.remove('fail');
     resp.classList.remove('send');
 
-    let url = `https://api.whatsapp.com/send?phone=573105010573&text=*_Barbería Líder_*%0A*Reservas*%0A%0A*¿Cuál es tu nombre?*%0A${cliente}%0A*Indica la fecha de tu reserva*%0A${fecha}%0A*Indica la hora de tu reserva*%0A${hora}%0A*Empleado de preferencia*%0A${empleado}%0A*¿Cuál es el servicio que se desea realizar?*%0A${servicio}`;
+    let url = `https://api.whatsapp.com/send?phone=${telefono}&text=*_Barbería Líder_*%0A*Reservas*%0A%0A*¿Cuál es tu nombre?*%0A${cliente}%0A*Indica la fecha de tu reserva*%0A${fecha}%0A*Indica la hora de tu reserva*%0A${hora}%0A*Empleado de preferencia*%0A${empleado}%0A*¿Cuál es el servicio que se desea realizar?*%0A${servicio}`;
 
     if(cliente === "" || fecha === "" || hora === ""){
         resp.classList.add('fail');

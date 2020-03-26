49aa684d4a602ae35fb2307a821bf2843688d285
ElisPerez <62060473+ElisPerez@users.noreply.github.com>
2020-03-26 13:04:08 -0500

    Update form.js

--- js/form.js
+++ js/form.js
@@ -11,7 +11,7 @@ document.querySelector('#submit').addEventListener('click', (e)=>{
     resp.classList.remove('fail');
     resp.classList.remove('send');
 
-    let url = `https://api.whatsapp.com/send?phone=573105010573&text=*_Barberia Lider_*%0A*Reservas*%0A%0A*¿Cual es tu nombre?*%0A${cliente}%0A*Indica la fecha de tu reserva*%0A${fecha}%0A*Indica la hora de tu reserva*%0A${hora}%0A*Barbero de preferencia*%0A${barbero}%0A*¿Cual es el servicio que se desea realizar?*%0A${servicio}`;
+    let url = `https://api.whatsapp.com/send?phone=573105010573&text=*_Barbería Líder_*%0A*Reservas*%0A%0A*¿Cuál es tu nombre?*%0A${cliente}%0A*Indica la fecha de tu reserva*%0A${fecha}%0A*Indica la hora de tu reserva*%0A${hora}%0A*Barbero de preferencia*%0A${barbero}%0A*¿Cuál es el servicio que se desea realizar?*%0A${servicio}`;
 
     if(cliente === "" || fecha === "" || hora === ""){
         resp.classList.add('fail');

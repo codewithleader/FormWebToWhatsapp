66b1a4f043152afc533071a78c3ae996b99f1513
unknown <ingsistemelisp@gmail.com>
2020-03-29 09:49:56 -0500

    Dando formato al form.js

--- js/form.js
+++ js/form.js
@@ -1,20 +1,20 @@
-document.querySelector('#submit').addEventListener('click', (e)=>{
-    e.preventDefault();
+document.querySelector("#submit").addEventListener("click", e => {
+  e.preventDefault();
 
-    //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
-	let telefono = "573105010573";
-    
-    let cliente = document.querySelector('#cliente').value;
-    let fecha = document.querySelector('#fecha').value;
-    let hora = document.querySelector('#hora').value;
-    let empleado = document.querySelector('#empleado').value;
-    let servicio = document.querySelector('#servicio').value;
-    let resp = document.querySelector('#respuesta');
+  //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+  let telefono = "573105010573";
 
-    resp.classList.remove('fail');
-    resp.classList.remove('send');
+  let cliente = document.querySelector("#cliente").value;
+  let fecha = document.querySelector("#fecha").value;
+  let hora = document.querySelector("#hora").value;
+  let empleado = document.querySelector("#empleado").value;
+  let servicio = document.querySelector("#servicio").value;
+  let resp = document.querySelector("#respuesta");
 
-    let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+  resp.classList.remove("fail");
+  resp.classList.remove("send");
+
+  let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
 		*_MI NEGOCIO_*%0A
 		*Reservas*%0A%0A
 		*¿Cuál es tu nombre?*%0A
@@ -28,14 +28,14 @@ document.querySelector('#submit').addEventListener('click', (e)=>{
 		*¿Cuál es el servicio que se desea realizar?*%0A
 		${servicio}`;
 
-    if(cliente === "" || fecha === "" || hora === ""){
-        resp.classList.add('fail');
-        resp.innerHTML= `Faltan algunos datos, ${cliente}`;
-        return false;
-    }
-    resp.classList.remove('fail');
-    resp.classList.add('send');
-    resp.innerHTML= `Se ha enviado tu reserva, ${cliente}`;
-    window.open(url);
+  if (cliente === "" || fecha === "" || hora === "") {
+    resp.classList.add("fail");
+    resp.innerHTML = `Faltan algunos datos, ${cliente}`;
+    return false;
+  }
+  resp.classList.remove("fail");
+  resp.classList.add("send");
+  resp.innerHTML = `Se ha enviado tu reserva, ${cliente}`;
 
+  window.open(url);
 });

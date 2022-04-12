9167c67c1bb8b46f5fe50226a6c38e0202d6c764
Elis Antonio Pérez <62060473+ElisPerez@users.noreply.github.com>
2022-04-12 14:46:43 -0500

    Update form.js
    cambio de numero telefonico.

--- js/form.js
+++ js/form.js
@@ -2,7 +2,7 @@ document.querySelector("#submit").addEventListener("click", e => {
   e.preventDefault();
 
   //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
-  let telefono = "573105010573";
+  let telefono = "573136283643";
 
   let cliente = document.querySelector("#cliente").value;
   let fecha = document.querySelector("#fecha").value;

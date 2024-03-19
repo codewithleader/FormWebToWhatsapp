afe6266edc23ffbf4b49c418e8f87da34a1a082c
Elis Antonio Perez <codewithleader@gmail.com>
2024-03-19 12:41:01 -0500

    feat: :sparkles: Negocio de Martina

--- js/form.js
+++ js/form.js
@@ -4,38 +4,64 @@ document.querySelector("#submit").addEventListener("click", e => {
   //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
   const telefono = "573136283643";
 
-  const cliente = document.querySelector("#cliente").value;
-  const fecha = document.querySelector("#fecha").value;
-  const hora = document.querySelector("#hora").value;
-  const empleado = document.querySelector("#empleado").value;
-  const servicio = document.querySelector("#servicio").value;
-  const resp = document.querySelector("#respuesta");
+  const nombre = document.querySelector("#nombre").value;
+  const razonsocial = document.querySelector("#razonsocial").value;
+  const CUIT = document.querySelector("#CUIT").value;
+  const nombrecomercio = document.querySelector("#nombrecomercio").value;
+  const direccion = document.querySelector("#direccion").value;
+  const localidad = document.querySelector("#localidad").value;
+  const provincia = document.querySelector("#provincia").value;
+  const tlf = document.querySelector("#telefono").value;
+  const email = document.querySelector("#email").value;
+  const local = document.querySelector("#local").value;
 
+  const resp = document.querySelector("#respuesta");
   resp.classList.remove("fail");
   resp.classList.remove("send");
 
   const url = `https://api.whatsapp.com/send?phone=${telefono}&text=
-		*_MI NEGOCIO_*%0A
-		*Reservas*%0A%0A
-		*¿Cuál es tu nombre?*%0A
-		${cliente}%0A
-		*Indica la fecha de tu reserva*%0A
-		${fecha}%0A
-		*Indica la hora de tu reserva*%0A
-		${hora}%0A
-		*Empleado de preferencia*%0A
-		${empleado}%0A
-		*¿Cuál es el servicio que se desea realizar?*%0A
-		${servicio}`;
+   	*_MI NEGOCIO_*%0A
+   	*Reservas*%0A%0A  
+    *¿CAMBIA ESTO?*%0A
+    ${nombre}%0A
+    *¿CAMBIA ESTO?*%0A
+    ${razonsocial}%0A
+    *¿CAMBIA ESTO?*%0A
+    ${CUIT}%0A
+    *¿CAMBIA ESTO?*%0A
+    ${nombrecomercio}%0A
+    *¿CAMBIA ESTO?*%0A
+    ${direccion}%0A
+    *¿CAMBIA ESTO?*%0A
+    ${localidad}%0A
+    *¿CAMBIA ESTO?*%0A
+    ${provincia}%0A
+    *¿CAMBIA ESTO?*%0A
+    ${tlf}%0A
+    *¿CAMBIA ESTO?*%0A
+    ${email}%0A
+    *¿CAMBIA ESTO?*%0A
+    ${local}`;
 
-  if (cliente === "" || fecha === "" || hora === "") {
+  if (
+    !nombre ||
+    !razonsocial ||
+    !CUIT ||
+    !nombrecomercio ||
+    !direccion ||
+    !localidad ||
+    !provincia ||
+    !tlf ||
+    !email ||
+    !local
+  ) {
     resp.classList.add("fail");
-    resp.innerHTML = `Faltan algunos datos, ${cliente}`;
+    resp.textContent = `Faltan algunos datos, ${nombre || ''}`;
     return false;
   }
   resp.classList.remove("fail");
   resp.classList.add("send");
-  resp.innerHTML = `Se ha enviado tu reserva, ${cliente}`;
+  resp.textContent = `Se ha enviado tu reserva, ${nombre || ''}`;
 
   window.open(url);
 });
